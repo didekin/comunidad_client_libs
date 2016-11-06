ff2e6ffb35fadfb7f6e1c70ebd5163b95bd6e0ca
Pedro Nevado <pedro@didekin.es>
2016-11-06 11:36:37 +0100

    version didekinlibs-a-0.1.2

---
+++ client_lib/buildtest.sh
@@ -0,0 +1,43 @@
+# It must be executed after 'cdlibs' with './client_lib/buildtest.sh environment version'
+# Environments: local, master
+
+#!/bin/bash
+
+export FUNCTIONS_FILE=terminal/envfunctions
+
+if ! [ -f $FUNCTIONS_FILE ]
+    then
+        echo "No environment functions file: $FUNCTIONS_FILE" 1>&2; exit 1
+    else
+        . $FUNCTIONS_FILE
+fi
+
+setArgsLibs "$1" "$2"
+
+./gradlew clean
+
+git checkout $ENV
+
+if [ $ENV = "$LOCAL_ENV" ] ; then
+    git add .
+    git commit -m  "version $VERSION"
+fi
+
+if [ $ENV = "$PRO_ENV" ] ; then
+    git merge "$LOCAL_ENV"  -m "version $VERSION"
+fi
+
+if [ $ENV = "$LOCAL_ENV" ] ; then
+    ./gradlew -Pversionjar=$VERSION_SUFFIX-$ENV build
+else
+    ./gradlew -Pversionjar=$VERSION_SUFFIX build
+fi
+echo "gradle client_lib build exit code = $?"
+
+git push $GITREMOTE $ENV
+
+#rm releases/$ENV/*
+mv client_lib/build/libs/*.jar releases/$ENV/
+
+git checkout local
+
