4b9cb7a80a872e460c8373e00e50cc0a19817437
Pedro Nevado <pedro@didekin.es>
2017-01-26 16:24:58 +0100

    version didekinlibs-a-0.1.11

---
+++ client_lib/src/main/java/com/didekin/common/gcm/GcmKeyValueData.java
@@ -0,0 +1,19 @@
+package com.didekin.common.gcm;
+
+/**
+ * User: pedro@didekin
+ * Date: 26/01/17
+ * Time: 16:21
+ */
+@SuppressWarnings("unused")
+public final class GcmKeyValueData {
+
+    /**
+     * The key to retrieve from the data payload the type of message.
+     */
+    String type_message_key = "typeMsg";
+
+    private GcmKeyValueData()
+    {
+    }
+}
