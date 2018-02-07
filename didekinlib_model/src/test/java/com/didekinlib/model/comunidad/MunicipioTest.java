df98080441e594178a1eb1dba39f620b8d23b073
Pedro Nevado <pedro@didekin.es>
2018-02-07 14:23:34 +0100

    version didekinlibs-a-0.4.3

--- didekinlib_model/src/test/java/com/didekinlib/model/comunidad/MunicipioTest.java
+++ didekinlib_model/src/test/java/com/didekinlib/model/comunidad/MunicipioTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 public class MunicipioTest {
 
     @Test
-    public void test_toString() throws Exception
+    public void test_toString()
     {
        Municipio municipio = new Municipio((short) 12, "municipio_12", new Provincia((short) 4, "provincia_4"));
        assertThat(municipio.getNombre(), is(municipio.toString()));
@@ -21,15 +21,15 @@ public class MunicipioTest {
 
     @SuppressWarnings("EqualsWithItself")
     @Test
-    public void testCompareTo() throws Exception
+    public void testCompareTo()
     {
         Municipio municipio_1 = new Municipio((short) 23,new Provincia((short)11));
         Municipio municipio_2 = new Municipio((short) 2,new Provincia((short)11));
         Municipio municipio_3 = new Municipio((short) 11,new Provincia((short)9));
 
         assertThat((municipio_1.equals(municipio_1)) == (municipio_1.compareTo(municipio_1) == 0), is(true));
-        assertThat(municipio_1.compareTo(municipio_2), is(1));
-        assertThat(municipio_2.compareTo(municipio_3), is(1));
-        assertThat(municipio_1.compareTo(municipio_3), is(1));
+        assertThat(municipio_1.compareTo(municipio_2) > 0, is(true));
+        assertThat(municipio_2.compareTo(municipio_3) > 0, is(true));
+        assertThat(municipio_1.compareTo(municipio_3) > 0, is(true));
     }
 }
