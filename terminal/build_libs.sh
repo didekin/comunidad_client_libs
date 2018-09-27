ff53638e2b95df90941a2944e2a07511a4d75b08
Pedro Nevado <pedro@didekin.es>
2018-09-27 10:29:04 +0200

    version didekinlibs-b.1.11

--- terminal/build_libs.sh
+++ terminal/build_libs.sh
@@ -8,8 +8,8 @@
 ./gradlew clean
 
 # Hay controlar si cada una de estas llamadas tiene éxito.
-source ./didekinlib_utilities/buildtest.sh
-#source ./didekinlib_model/buildtest.sh $1 $2 $1
+source ./didekinlib_utilities/buildtest.sh $1 $2
+source ./didekinlib_model/buildtest.sh $1 $2
 
 echo "================== FINAL!!!: build_libs finish, exit code = $? =============="
 
